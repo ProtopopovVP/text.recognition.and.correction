56d9f68ed8e10b7d3f7ee49b8351135abfbcc68d
navaroe <61462652+navaroe@users.noreply.github.com>
2022-02-14 13:07:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread('image1.png')
+img = cv2.imread('data/image3.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 config = r'--oem 3 --psm 6'
@@ -11,4 +11,3 @@ pytesseract.image_to_string(img, config=config)
 
 print(pytesseract.image_to_string(img, config=config))
 
-print(img)
