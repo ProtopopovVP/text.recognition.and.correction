30a0dcb91acff6459056da9ecdc075fb614b5bff
navaroe <61462652+navaroe@users.noreply.github.com>
2022-02-21 07:42:14 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import cv2
-import pytesseract
+import cv2, pytesseract, enchant, difflib
+from enchant.checker import SpellChecker
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -7,7 +7,23 @@ img = cv2.imread('data/image3.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 config = r'--oem 3 --psm 6'
-pytesseract.image_to_string(img, config=config)
+recognized_string = pytesseract.image_to_string(img, config=config)
 
-print(pytesseract.image_to_string(img, config=config))
+corrected_string = recognized_string
 
+checker = SpellChecker("en_US")
+dictionary = enchant.Dict("en_US")
+checker.set_text(corrected_string)
+
+for i in checker:
+    woi = i.word;
+    suggestions = set(dictionary.suggest(woi))
+    sim = dict()
+    for word in suggestions:
+        measure = difflib.SequenceMatcher(None, woi, word).ratio()
+        sim[measure] = word
+    suggested_word = sim[max(sim.keys())]
+    corrected_string = corrected_string.replace(woi, suggested_word)
+
+print(recognized_string)
+print(corrected_string)
